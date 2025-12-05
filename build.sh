87433890c968053b51992b1a0182409c5b9ea0d8
Grant Morris <grant.morris.dev@gmail.com>
2025-12-05 12:05:31 -0600

    Update source file names to use platform_ convention

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-clang ./src/main.c -o game -I/usr/include/SDL3 -lSDL3
+clang -std=c99 ./src/platform_sdl.c -o game -I/usr/include/SDL3 -lSDL3
--- src/platform_sdl.c
+++ src/platform_sdl.c
@@ -1,14 +1,13 @@
 #define SDL_MAIN_USE_CALLBACKS 1
-
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
 #include "types.h"
 
-#include "iterate.c"
-#include "init.c"
-#include "event.c"
-#include "quit.c"
+#include "platform_iterate.c"
+#include "platform_init.c"
+#include "platform_event.c"
+#include "platform_quit.c"
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char *argv[])
 {
